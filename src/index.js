f4defd78431df09815ca8880bedd86a1db3f7f09
tanulka <tanulkako@gmail.com>
2021-08-27 15:29:17 +0300

    Created Dark/Light Mode Switch

--- src/index.js
+++ src/index.js
@@ -21,3 +21,32 @@ import aaa from './js/db'
 // ----------------------------------------------------------------------------
 
 //
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// }
+const bodyRef = document.body
+
+const inputEl = document.getElementById('theme-switch-toggle')
+
+inputEl.addEventListener('change', () => {
+  if (inputEl.checked) {
+    bodyRef.classList.add('dark-theme')
+    bodyRef.classList.remove('light-theme')
+    localStorage.setItem('theme', 'dark-theme')
+  } else {
+    bodyRef.classList.add('light-theme')
+    bodyRef.classList.remove('dark-theme')
+    localStorage.setItem('theme', 'light-theme')
+  }
+})
+
+let className = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
+
+if (className) {
+  bodyRef.setAttribute('class', className)
+
+  if (className === 'dark-theme') {
+    inputEl.checked = 'true'
+  }
+}
