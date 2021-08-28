29f4ac6b962a217867fc5abd7ea7cd5b12fcb597
tanulka <tanulkako@gmail.com>
2021-08-28 09:23:22 +0300

    clean up code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import menuArr from './menu.json'
-import menuItem from './templates/menu-items.hbs'
+import menuArr from './menu.json';
+import menuItem from './templates/menu-items.hbs';
 
-const bodyRef = document.body
+const bodyRef = document.body;
 
-const inputEl = document.getElementById('theme-switch-toggle')
+const inputEl = document.getElementById('theme-switch-toggle');
 
 inputEl.addEventListener('change', () => {
   if (inputEl.checked) {
-    bodyRef.classList.add('dark-theme')
-    bodyRef.classList.remove('light-theme')
-    localStorage.setItem('theme', 'dark-theme')
+    bodyRef.classList.add('dark-theme');
+    bodyRef.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark-theme');
   } else {
-    bodyRef.classList.add('light-theme')
-    bodyRef.classList.remove('dark-theme')
-    localStorage.setItem('theme', 'light-theme')
+    bodyRef.classList.add('light-theme');
+    bodyRef.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light-theme');
   }
-})
+});
 
-let className = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
+let className = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 if (className) {
-  bodyRef.setAttribute('class', className)
+  bodyRef.setAttribute('class', className);
 
   if (className === 'dark-theme') {
-    inputEl.checked = 'true'
+    inputEl.checked = 'true';
   }
 }
 
-const menuRef = document.querySelector('.js-menu')
-const markup = menuItem(menuArr)
-menuRef.insertAdjacentHTML('beforeend', markup)
+const menuRef = document.querySelector('.js-menu');
+const markup = menuItem(menuArr);
+menuRef.insertAdjacentHTML('beforeend', markup);
